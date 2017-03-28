a1547c5fa5a29a52af0fee843f3c800b083170eb
Eivind Teig <eivind.teig@gmail.com>
2017-03-28 14:58:22 +0200

    translate

---
+++ translateex.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from google.cloud import translate
+
+
+def translate_text(text, target='en'):
+    translate_client = translate.Client()
+    result = translate_client.translate(text, target_language=target)
+    print('Text: ', result['input'])
+    print('Translation: ', result['translatedText'])
+    print('Detected source language: ', result['detectedSourceLanguage'])
+
+example_text = '''Hei hvordan går det?'''
+
+translate_text(translate_text)
