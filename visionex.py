00a11a6cb7c129020ff64b4a9b607ffd96b87feb
Eivind Teig <eivind.teig@gmail.com>
2017-03-28 13:15:34 +0200

    typo

--- visionex.py
+++ visionex.py
@@ -1,15 +1,14 @@
 import io
-from google.cloud import  vision
+from google.cloud import vision
 
 vision_client = vision.Client()
 file_name = 'orthopedic-shoes.jpg'
 
 with io.open(file_name, 'rb') as image_file:
-	content = image_file.read()
-	image = vision_client.image(content=content)
+    content = image_file.read()
+    image = vision_client.image(content=content)
 
 labels = image.detect_labels()
 
 for label in labels:
-	print(label.description)
-
+    print(label.description)
