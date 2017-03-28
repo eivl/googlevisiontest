9d04e015ad5841a405fd23e6420e4b81ade476cc
Eivind Teig <eivind.teig@gmail.com>
2017-03-28 14:28:35 +0200

    language api

---
+++ natlangex.py
@@ -0,0 +1,12 @@
+from google.cloud import language
+
+
+def language_analysis(text):
+    client = language.Client()
+    document = client.document_from_text(text)
+    sent_analysis = document.analyze_sentiment()
+    print(dir(sent_analysis))
+    sentiment = sent_analysis.sentiment
+    ent_analysis = document.analyze_entities()
+    entities = ent_analysis.entities
+    return sentiment, entities
