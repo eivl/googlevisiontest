08c814760cb53fe1c38bb62c9d891e617c196140
Eivind Teig <eivind.teig@gmail.com>
2017-03-29 08:42:20 +0200

    more bug fixes

--- translateex.py
+++ translateex.py
@@ -3,15 +3,19 @@ from google.cloud import translate
 
 
 def translate_text(text, target='en'):
+    """
+    Target must be an ISO 639-1 language code.
+    https://cloud.google.com/translate/docs/languages
+    """
     translate_client = translate.Client()
     result = translate_client.translate(
         text,
         target_language=target)
 
-    print('Text: ', result['input'])
-    print('Translation: ', result['translatedText'])
-    print('Detected source language: ', result['detectedSourceLanguage'])
+    print(u'Text: {}'.format(result['input']))
+    print(u'Translation: {}'.format(result['translatedText']))
+    print(u'Detected source language: {}'.format(
+        result['detectedSourceLanguage']))
 
-example_text = '''Hei hvordan går det?'''
-
-translate_text(translate_text)
+example_text = '''Hola saludos desde Colombia excelentes tutoriales me gustaría poder por lo menos tener los subtitulos ene español ...excelente gracias por compartir tus conocimientos'''
+translate_text(example_text.decode('utf-8'), target='en')
