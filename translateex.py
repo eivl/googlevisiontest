4025256eb4c8126b3b0190d7fbe9f315a8fdfa39
Eivind Teig <eivind.teig@gmail.com>
2017-03-29 08:49:53 +0200

    norsk

--- translateex.py
+++ translateex.py
@@ -20,5 +20,5 @@ def translate_text(text, target='en'):
     print(u'Detected source language: {}'.format(
         result['detectedSourceLanguage']))
 
-example_text = '''Hola saludos desde Colombia excelentes tutoriales me gustaría poder por lo menos tener los subtitulos ene español ...excelente gracias por compartir tus conocimientos'''
+example_text = '''Hei har du det bra?'''
 translate_text(example_text.decode('utf-8'), target='en')
