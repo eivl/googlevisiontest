b6ae8d69b0d2149084310a3aeb74abe0b0a25606
Eivind Teig <eivind.teig@gmail.com>
2017-03-29 08:48:42 +0200

    sys utf-8

--- translateex.py
+++ translateex.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from google.cloud import translate
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 def translate_text(text, target='en'):
@@ -17,5 +20,5 @@ def translate_text(text, target='en'):
     print(u'Detected source language: {}'.format(
         result['detectedSourceLanguage']))
 
-example_text = '''Hola saludos desde Colombia excelentes tutoriales me gustaría poder por lo menos tener los subtitulos ene español ...excelente gracias por compartir tus conocimientos'''.encode('utf-8')
+example_text = '''Hola saludos desde Colombia excelentes tutoriales me gustaría poder por lo menos tener los subtitulos ene español ...excelente gracias por compartir tus conocimientos'''
 translate_text(example_text.decode('utf-8'), target='en')
